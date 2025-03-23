fb9c2b657c8bb1ab1741a6be072edc5160961036
Isac Canedo <32867124+isaccanedo@users.noreply.github.com>
2025-03-23 15:35:27 -0300

    Update SpringBootMysqlRestApiTutorialApplicationTests.java

--- src/test/java/br/com/isaccanedo/SpringBootMysqlRestApiTutorialApplicationTests.java
+++ src/test/java/br/com/isaccanedo/SpringBootMysqlRestApiTutorialApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringBootMysqlRestApiTutorialApplicationTests {
 
+	// Classe de testes
 	@Test
 	void contextLoads() {
 	}
